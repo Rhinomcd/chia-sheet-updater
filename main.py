ffad5153d4cdc4589d0042438d60c4d6847b02b2
Rhinomcd <rhinomcd@gmail.com>
2021-05-11 13:24:17 -0700

    Add chia output sniffing

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from sheetfu import SpreadsheetApp
 from datetime import datetime
+import subprocess
+import re
 
 SPREADSHEET_ID = '1G7oNS9OHewTztoEFlqR43qYTQNNJipUJcJq6nqp3Bas'
 SHEET_NAME = 'Ryan Hax'
 
+
 def main():
     sa = SpreadsheetApp('secret.json')
     spreadsheet = sa.open_by_id(SPREADSHEET_ID)
-    sheet =  spreadsheet.get_sheet_by_name(SHEET_NAME)
+    sheet = spreadsheet.get_sheet_by_name(SHEET_NAME)
 
     current_plot_cell = sheet.get_range_from_a1(a1_notification='A2')
     current_plot_cell.set_value(get_plot_count())
@@ -15,8 +18,13 @@ def main():
     last_updated_cell = sheet.get_range_from_a1(a1_notification='B2')
     last_updated_cell.set_value(datetime.now().isoformat())
 
+
 def get_plot_count():
-    return 5
+    pattern = re.compile('Plot count: (?P<plotcount>\d+)')
+    farm_summary = subprocess.run(['chia', 'farm', 'summary'],
+                                  capture_output=True).stdout
+    match = re.search(pattern, str(farm_summary))
+    return match.group('plotcount')
 
 
 if __name__ == '__main__':
