51c42a63ac85ff81c4663492f4e56ecbb1247c10
Rhinomcd <rhinomcd@gmail.com>
2021-05-12 12:15:26 -0700

    Cast plot count to int

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     sheet = spreadsheet.get_sheet_by_name(SHEET_NAME)
 
     current_plot_cell = sheet.get_range_from_a1(a1_notification='A2')
-    current_plot_cell.set_value(get_plot_count())
+    current_plot_cell.set_value(int(get_plot_count()))
 
     last_updated_cell = sheet.get_range_from_a1(a1_notification='B2')
     last_updated_cell.set_value(datetime.now().isoformat())
